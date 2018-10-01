1cd858eacbc7f0388f82ccf416ec3473dbd07c6b
solodynamo <bittuf3@gmail.com>
2018-10-02 03:15:15 +0530

    Use Doubly linked list container for tasks

--- poolboy.go
+++ poolboy.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"runtime"
 	"sync/atomic"
+	"container/list"
 );
 
 type Signal struct{};
@@ -18,14 +19,21 @@ type Worker struct {
 }
 
 type Pool struct {
-	workers chan *Worker
+	tasks        *DLL
+	workers      *DLL
 	destroy chan Signal
-	mx sync.Mutex
-	tasks chan fun
+	mx *sync.Mutex
+	wg *sync.WaitGroup
+	freeSignal chan Signal
+	launchSignal chan Signal
 	capacity int32
 	running int32
 };
 
+type DLL struct {
+	doublylinkedlist *list.List
+	m     sync.Mutex
+}
 
 const DEFAULT_ROUTINE_POOL_SIZE = math.MaxInt32;
 var gopherPool = NewPool(DEFAULT_ROUTINE_POOL_SIZE);
@@ -36,60 +44,72 @@ func (w *Worker) run() {
 		for {
 			select {
 			case f := <-w.task:
-				f();
-				w.pool.workers <- w;
+				f()
+				w.pool.workers.push(w)
+				w.pool.wg.Done()
 			case <-w.exit:
-				atomic.AddInt32(&w.pool.running, -1);
-				return;
+				atomic.AddInt32(&w.pool.running, -1)
+				return
 			}
 		}
 	}()
 }
 
 func (w *Worker) stop() {
-	w.exit <- Signal{};
+	w.exit <- Signal{}
 }
 
 func (w *Worker) sendTask(task fun) {
-	w.task <- task;
+	w.task <- task
 }
 
 func Push(task fun) error {
-	return gopherPool.Push(task);
+	return gopherPool.Push(task)
 }
 
 func SwimmingGophers() int {
-	return gopherPool.Running();
+	return gopherPool.Running()
+}
+
+func WaitingGophers() {
+	gopherPool.Wait()
 }
 
 func PoolCapacity() int {
-	return gopherPool.Capacity();
+	return gopherPool.Capacity()
 }
 
 func FreeGopherSwimmers() int {
-	return gopherPool.Free();
+	return gopherPool.Free()
 }
 
 func NewPool(size int32) *Pool {
 	p := &Pool{
 		capacity: size,
-		tasks: make(chan fun, math.MaxInt32),
-		workers: make(chan *Worker, size),
+		tasks: NewDLL(),
+		workers: NewDLL(),
+		freeSignal:   make(chan Signal, math.MaxInt32),
+		launchSignal: make(chan Signal, math.MaxInt32),
 		destroy: make(chan Signal, runtime.GOMAXPROCS(-1)),
+		wg:           &sync.WaitGroup{},
 	};
 	p.loop(); 
 	return p;
 }
 
 func (p *Pool) loop() {
+	/*
+	* Don't alter the no of threads that will be used out of system for running go code by passing -1.
+	* Spawn go routines till no of default allocated OS threads reached.
+	**/
 	for i :=0; i < runtime.GOMAXPROCS(-1); i++ {
 		go func() {
 			for {
 				select {
-				case task := <-p.tasks:
-					p.getWorker().sendTask(task)
+				case <-p.launchSignal:
+					p.getWorker().sendTask(p.tasks.pop().(fun))
 				case <-p.destroy:
-					return;
+					return
 				}
 			}
 		}()
@@ -97,61 +117,89 @@ func (p *Pool) loop() {
 }
 
 func (p *Pool) Running() int {
-	swimmingGophers := int(atomic.LoadInt32(&p.running));
-	return swimmingGophers;
+	swimmingGophers := int(atomic.LoadInt32(&p.running))
+	return swimmingGophers
 }
 
 func (p *Pool) Free() int {
 	idleGophers := int(atomic.LoadInt32(&p.capacity) - atomic.LoadInt32(&p.running));
-	return idleGophers;
+	return idleGophers
 }
 func (p *Pool) Push(task fun) error {
 	if len(p.destroy) > 0 {
-		return nil;
+		return nil
 	}
-	p.tasks <- task;
+	p.tasks.push(task)
+	p.launchSignal <- Signal{}
+	p.wg.Add(1)
 	return nil
 }
 
 func (p *Pool) Capacity() int {
-	return int(atomic.LoadInt32(&p.capacity));
+	return int(atomic.LoadInt32(&p.capacity))
 }
 
 func (p *Pool) Destroy() error {
-	p.mx.Lock();
-	defer p.mx.Unlock();
+	p.mx.Lock()
+	defer p.mx.Unlock()
 	for i:=0; i < runtime.GOMAXPROCS(-1) +1; i++ {
-		p.destroy <- Signal{};
+		p.destroy <- Signal{}
 	}
-	return nil;
+	return nil
+}
+
+func (p *Pool) Wait() {
+	p.wg.Wait()
 }
 
 func (p *Pool) reachLimit() bool {
-	return p.Running() >= p.Capacity();
+	return p.Running() >= p.Capacity()
 }
 
 func (p *Pool) newWorker() *Worker {
+	if p.reachLimit() {
+		<-p.freeSignal
+		return p.getWorker()
+	}
 	worker := &Worker{
 		pool: p,
 		task: make(chan fun),
 		exit: make(chan Signal),
 	}
-	worker.run();
-	return worker;
+	worker.run()
+	return worker
 }
 
 func (p *Pool) getWorker() *Worker {
-	var worker *Worker;
 	defer atomic.AddInt32(&p.running,1);
-	if p.reachLimit() {
-		worker = <-p.workers;
-	} else {
-		select {
-		case worker = <-p.workers:
-			return worker;
-		default:
-			worker = p.newWorker();
-		}
+	if w := p.workers.pop(); w != nil {
+		return w.(*Worker)
 	}
-	return worker;
+	return p.newWorker()
+}
+
+func (p *Pool) PutWorker(worker *Worker) {
+	p.workers.push(worker)
+	if p.reachLimit() {
+		p.freeSignal <- Signal{}
+	}
+}
+
+ func NewDLL() *DLL {
+	q := new(DLL)
+	q.doublylinkedlist = list.New()
+	return q
+}
+ func (q *DLL) push(v interface{}) {
+	defer q.m.Unlock()
+	q.m.Lock()
+	q.doublylinkedlist.PushFront(v)
+}
+ func (q *DLL) pop() interface{} {
+	defer q.m.Unlock()
+	q.m.Lock()
+	if elem := q.doublylinkedlist.Back(); elem != nil{
+		return q.doublylinkedlist.Remove(elem)
+	}
+	return nil
 }
