1ab2826f9dc8ac74088cad7e315b1760f45a1788
solodynamo <bittuf3@gmail.com>
2018-10-02 20:39:20 +0530

    Adds license and adds timer to see realtime allocated routines and total capacity

--- poolboy.go
+++ poolboy.go
@@ -1,17 +1,20 @@
-package poolboy;
+package poolboy
 
 import (
+	"container/list"
+	"errors"
+	"fmt"
 	"log"
-	"sync"
 	"math"
 	"runtime"
+	"sync"
 	"sync/atomic"
-	"container/list"
-);
+	"time"
+)
 
-type Signal struct{};
+type Signal struct{}
 
-type fun func();
+type fun func()
 
 type Worker struct {
 	pool *Pool
@@ -22,38 +25,51 @@ type Worker struct {
 type Pool struct {
 	tasks        *DLL
 	workers      *DLL
-	destroy chan Signal
-	mx *sync.Mutex
-	wg *sync.WaitGroup
-	freeSignal chan Signal
+	destroy      chan Signal
+	statTime    time.Duration  // time to display stats.
+	mx           *sync.Mutex
+	wg           *sync.WaitGroup
+	freeSignal   chan Signal
 	launchSignal chan Signal
-	capacity int32
-	running int32
-	logFunc func(message string)
-};
+	capacity     int32
+	running      int32
+	logFunc      func(message string)
+}
 
 type DLL struct {
 	doublylinkedlist *list.List
-	m     sync.Mutex
+	m                sync.Mutex
 }
 
-const DEFAULT_ROUTINE_POOL_SIZE = math.MaxInt32;
+const DEFAULT_ROUTINE_POOL_SIZE = math.MaxInt32
+
+var (
+	ErrorInvalidMinRoutines = errors.New("Invalid minimum number of routines")
+	ErrorInvalidStatTime = errors.New("Invalid duration for stat time")
+	ErrNoTaskInQ = errors.New("No task to run")
+	ErrCapacity = errors.New("Thread Pool At Capacity")
+)
 
 func logFunc(message string) {
 	log.Println(message)
 }
 var once sync.Once
 
+func init() {
+	log.SetPrefix("TRACE: ")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
 func limitThreadsAccToCPUCores() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-func getIntOnce() *Pool{
-	var gpool * Pool
+func getIntOnce() *Pool {
+	var gpool *Pool
 	limitThreadsAccToCPUCores()
 	once.Do(func() {
 		log.Println("Initializing Pool")
-		gpool = NewPool(DEFAULT_ROUTINE_POOL_SIZE,logFunc)
+		gpool, _ = NewPool(DEFAULT_ROUTINE_POOL_SIZE, time.Second, logFunc)
 	})
 	return gpool
 }
@@ -67,6 +83,10 @@ func (w *Worker) run() {
 			case f := <-w.task:
 				f()
 				w.pool.workers.push(w)
+				atomic.AddInt32(&w.pool.running,1)
+				if atomic.AddInt32(&w.pool.running, 0) == w.pool.capacity {
+					fmt.Println(ErrCapacity)
+				}
 				w.pool.wg.Done()
 			case <-w.exit:
 				atomic.AddInt32(&w.pool.running, -1)
@@ -84,19 +104,28 @@ func (w *Worker) sendTask(task fun) {
 	w.task <- task
 }
 
-func NewPool(size int32, logFunc func(message string)) *Pool {
+func NewPool(size int32, statTime time.Duration, logFunc func(message string)) (*Pool, error) {
+	if size <= 0 {
+		return nil, ErrorInvalidMinRoutines
+	}
+
+	if statTime < time.Millisecond {
+		return nil, ErrorInvalidStatTime
+	}
+
 	p := &Pool{
-		capacity: size,
-		tasks: NewDLL(),
-		workers: NewDLL(),
+		capacity:     size,
+		tasks:        NewDLL(),
+		workers:      NewDLL(),
 		freeSignal:   make(chan Signal, math.MaxInt32),
 		launchSignal: make(chan Signal, math.MaxInt32),
-		destroy: make(chan Signal, runtime.GOMAXPROCS(-1)),
+		destroy:      make(chan Signal, runtime.GOMAXPROCS(-1)),
+		statTime:    statTime,
 		wg:           &sync.WaitGroup{},
-		logFunc:logFunc,
-	};
-	p.loop();
-	return p;
+		logFunc:      logFunc,
+	}
+	p.loop()
+	return p,nil
 }
 
 func Push(task fun) error {
@@ -119,20 +148,35 @@ func FreeGopherSwimmers() int {
 	return gopherPool.Free()
 }
 
-
 func (p *Pool) loop() {
 	/*
 	* Don't alter the no of threads that will be used out of system for running go code by passing -1.
 	* Spawn go routines till no of default allocated OS threads reached.
 	**/
-	for i :=0; i < runtime.GOMAXPROCS(-1); i++ {
+	for i := 0; i < runtime.GOMAXPROCS(-1); i++ {
 		go func() {
+			timer := time.NewTimer(p.statTime)
 			for {
 				select {
 				case <-p.launchSignal:
+					noOfTasks := p.tasks.doublylinkedlist.Len()
+					if noOfTasks <= 0 {
+						panic(ErrNoTaskInQ)
+					}
 					p.getWorker().sendTask(p.tasks.pop().(fun))
+
 				case <-p.destroy:
 					return
+				case <-timer.C:
+					// Capture the stats.
+					routines := atomic.LoadInt32(&p.running)
+					capacity := atomic.LoadInt32(&p.capacity)
+
+					// Display the stats.
+					p.log(fmt.Sprintf("Pool : Stats : A[%d] C[%d] ", routines, capacity))
+
+					// Reset the clock.
+					timer.Reset(p.statTime)
 				}
 			}
 		}()
@@ -145,7 +189,7 @@ func (p *Pool) Running() int {
 }
 
 func (p *Pool) Free() int {
-	idleGophers := int(atomic.LoadInt32(&p.capacity) - atomic.LoadInt32(&p.running));
+	idleGophers := int(atomic.LoadInt32(&p.capacity) - atomic.LoadInt32(&p.running))
 	return idleGophers
 }
 func (p *Pool) Push(task fun) error {
@@ -165,7 +209,8 @@ func (p *Pool) Capacity() int {
 func (p *Pool) Destroy() error {
 	p.mx.Lock()
 	defer p.mx.Unlock()
-	for i:=0; i < runtime.GOMAXPROCS(-1) +1; i++ {
+	routines := int(atomic.LoadInt32(&p.running))
+	for i := 0; i < routines; i++ {
 		p.destroy <- Signal{}
 	}
 	return nil
@@ -194,7 +239,7 @@ func (p *Pool) newWorker() *Worker {
 }
 
 func (p *Pool) getWorker() *Worker {
-	defer atomic.AddInt32(&p.running,1);
+	defer atomic.AddInt32(&p.running, 1)
 	if w := p.workers.pop(); w != nil {
 		return w.(*Worker)
 	}
@@ -208,20 +253,20 @@ func (p *Pool) PutWorker(worker *Worker) {
 	}
 }
 
- func NewDLL() *DLL {
+func NewDLL() *DLL {
 	q := new(DLL)
 	q.doublylinkedlist = list.New()
 	return q
 }
- func (q *DLL) push(v interface{}) {
+func (q *DLL) push(v interface{}) {
 	defer q.m.Unlock()
 	q.m.Lock()
 	q.doublylinkedlist.PushFront(v)
 }
- func (q *DLL) pop() interface{} {
+func (q *DLL) pop() interface{} {
 	defer q.m.Unlock()
 	q.m.Lock()
-	if elem := q.doublylinkedlist.Back(); elem != nil{
+	if elem := q.doublylinkedlist.Back(); elem != nil {
 		return q.doublylinkedlist.Remove(elem)
 	}
 	return nil
--- poolboy_test.go
+++ poolboy_test.go
@@ -20,11 +20,11 @@ func TestGopherPool(t *testing.T) {
 	for i := 0; i < n; i++ {
 		poolboy.Push(doLotOfWorkFun)
 	}
-	fmt.Println("____Stats____");
+	fmt.Println("____Stats____")
  	t.Log("PoolCapacity", poolboy.PoolCapacity())
 	t.Log("SwimmingGophers", poolboy.SwimmingGophers())
 	t.Log("FreeGopherSwimmers", poolboy.FreeGopherSwimmers())
-	poolboy.WaitingGophers();
+	poolboy.WaitingGophers()
  	mem := runtime.MemStats{}
 	runtime.ReadMemStats(&mem)
 	t.Log("memory usage:", mem.TotalAlloc/1024)
